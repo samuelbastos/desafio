6911ca7d1e52e3cdf36041b90bdbbfd8b8ca321c
samuelbastos <samjr015@gmail.com>
2016-09-13 19:33:51 -0300

    Melhoria no cálculo de amount ; Textos dos inputs retirados

--- feira.py
+++ feira.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from decimal import Decimal
 
-qtd_teste = input("Quantidade de casos teste: ")
+qtd_teste = input()
 
 for i in range( int(qtd_teste) ):
 
@@ -8,29 +9,22 @@ for i in range( int(qtd_teste) ):
 	produto_preco_dict = {}
 	produto_qtd_dict   = {}
 
-	qtd_produto_feira = input("Quantidade de produtos disponíveis na feira: ")
+	qtd_produto_feira = input()
 
 	for j in range( int(qtd_produto_feira) ):
 
-		produto_preco = input("Produto e preço: ").split()
+		produto_preco = input().split()
 		produto_preco_dict.setdefault(produto_preco[0], float(produto_preco[1]))
 
-	qtd_produto_desejado = input("Quantidade de produtos desejada: ")
+	qtd_produto_desejado = input()
 
 	for k in range( int(qtd_produto_desejado) ):
 		
-		produto_qtd = input("Produto e respectiva quantidade desejada: ").split()
+		produto_qtd = input().split()
 		produto_qtd_dict.setdefault(produto_qtd[0], int(produto_qtd[1]))
 
 	for x in produto_qtd_dict.keys():
-		for y in produto_preco_dict.keys():
 
-			if x == y:
-				amount = amount + produto_preco_dict.get(x)*produto_qtd_dict.get(x)
-
+		amount = amount + produto_preco_dict.get(x)*produto_qtd_dict.get(x)
 
 	print("R$", round(Decimal(amount),2))
-
-
-
-
