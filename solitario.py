d62494619dca8b50a27e54ea743cff24214c82cf
samuelbastos <samjr015@gmail.com>
2016-09-13 19:32:08 -0300

    Usando sort do próprio python ; lista convertida para int antes da ordenação

--- solitario.py
+++ solitario.py
@@ -1,53 +1,21 @@
 # -*- coding: utf-8 -*-
-
-def mergeSort(alist):
-
-    if len(alist)>1:
-        mid = len(alist)//2
-        lefthalf = alist[:mid]
-        righthalf = alist[mid:]
-
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
-
-        i=0
-        j=0
-        k=0
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                alist[k]=lefthalf[i]
-                i=i+1
-            else:
-                alist[k]=righthalf[j]
-                j=j+1
-            k=k+1
-
-        while i < len(lefthalf):
-            alist[k]=lefthalf[i]
-            i=i+1
-            k=k+1
-
-        while j < len(righthalf):
-            alist[k]=righthalf[j]
-            j=j+1
-            k=k+1
-
 flag = True 
 
 while flag:
 
-    b = input("Defina o tamanho do vetor: ")
+    b = input()
 
     if b == '0':
         break
 
-    a = input("Digite o vetor: ").split()
+    a = input().split()
+    a = [int(x) for x in a]
 
     if int(b) != len(a):
         print("Entradas não dão match")
         continue
 
-    mergeSort(a)
+    a.sort()
 
     counter = 0
     iterator = 0
